e85b600998aae2d56fe42c6ec87c27fbb7f5aab2
Bjorn 'Bjeaurn <bjorn@somnu.com>
2021-04-24 12:21:22 +0200

    feat: New theme, started on the main, got an asset.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import { Config, DEFAULT_CONFIG, Gine, IConfigArguments } from 'gine'
 import { MainScene } from './scenes/main'
 
 const cfg: Config = new Config(
-    document.querySelector('#game') as HTMLCanvasElement,
-    Object.assign(DEFAULT_CONFIG, {
-        width: 300,
-        height: 200
-    } as IConfigArguments)
+  document.querySelector("#game") as HTMLCanvasElement,
+  Object.assign(DEFAULT_CONFIG, {
+    width: 300,
+    height: 200,
+    tileSize: 32,
+  } as IConfigArguments)
 )
 const game = new Gine(cfg)
 
-console.log(Gine.store)
+Gine.store.image("dwarf", "assets/dwarf.png", {})
 
+console.log(Gine.store.getImage("dwarf"))
 const mainScene = new MainScene()
 game.changeScene(mainScene)
 game.start()
--- theme.md
+++ theme.md
@@ -1,4 +1,5 @@
 # Deeper and deeper
 RPG/Roguelike
 
-You are a dwarf, having to go deep into your own mines to save your mine. I guess?
+You are a dwarf, a team of fellow dwarves has lost contact with the main base. Last known position, deep inside the mines. You have been volunteered to find out what happened.
+
