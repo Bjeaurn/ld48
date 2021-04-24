2991970cc09c19a55c337bf286fa154a0a63c44b
Bjorn 'Bjeaurn <bjorn@somnu.com>
2021-04-24 19:41:35 +0200

    feat(levels): Added a bunch of levels for faster iteration development.

--- src/map.ts
+++ src/map.ts
@@ -4,14 +4,18 @@ import * as level0 from './levels/level0.json'
 import * as level1 from './levels/level1.json'
 import * as level2 from './levels/level2.json'
 import * as level3 from './levels/level3.json'
+import * as level4 from './levels/level4.json'
+import * as level5 from './levels/level5.json'
+import * as level6 from './levels/level6.json'
+import * as level7 from './levels/level7.json'
 
 export class Map {
-  levels = [level0, level1, level2, level3]
+  levels = [level0, level1, level2, level3, level4, level5, level6, level7]
   maxY: number = Math.ceil(Gine.CONFIG.width / Gine.CONFIG.tileSize)
   map: number[]
   entities: any[]
   constructor() {
-    this.loadLevel(3)
+    this.loadLevel(4)
   }
 
   getPosXY(x: number, y: number): number {
